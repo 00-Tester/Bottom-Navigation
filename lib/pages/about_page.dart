695b6b500cb979cb625c9d38b9225acbdd387826
John Mike Ignacio <166836142+00-Tester@users.noreply.github.com>
2025-09-18 17:59:06 +0800

    Flutter Bottom Navigation

---
+++ lib/pages/about_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AboutPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('ABOUT ME'),
+          backgroundColor: Colors.blue,
+          centerTitle: true,
+        ),
+    );
+  }
+}
+
+
---
+++ lib/pages/contact_page.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+
+class ContactPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('CONTACT US'),
+          backgroundColor: Colors.blue,
+          centerTitle: true,
+        ),
+    );
+  }
+}
